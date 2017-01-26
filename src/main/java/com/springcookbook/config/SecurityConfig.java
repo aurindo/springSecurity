06ab05e6c29c42138fba68517544fe3881a27323
Aurindo Jorge <aurindo_jorge@atlantico.com.br>
2017-01-25 23:09:26 -0300

    Fix login/logout with user and admin roles

--- src/main/java/com/springcookbook/config/SecurityConfig.java
+++ src/main/java/com/springcookbook/config/SecurityConfig.java
@@ -16,14 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			throws Exception {
 		  auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
 		  auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-		  auth.inMemoryAuthentication().withUser("dba").password("dba").roles("DBA");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		  http.authorizeRequests()
 			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-			.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+			.antMatchers("/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 			.and().formLogin();
 	}
 
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,7 +1,11 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@page session="false"%>
+
 <html>
 <body>
 	<h1>Title : ${title}</h1>
 	<h1>Message : ${message}</h1>
+	
+	<a href="<c:url value="/logout" />">Logout</a>
 </body>
 </html>
