3ef2e15013893a550a90a9f63f500122178eccf4
Aurindo Jorge <aurindo_jorge@atlantico.com.br>
2017-01-28 14:24:29 -0300

    Add authentication by database using spring jdbc template.

---
+++ src/main/java/com/springcookbook/component/CustomAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.springcookbook.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import com.springcookbook.service.AuthenticationService;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+	@Autowired
+	AuthenticationService authenticationService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        
+        return authenticationService.authentication(name, password);
+    }
+ 
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
